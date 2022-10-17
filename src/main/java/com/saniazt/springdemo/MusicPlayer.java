6fc123f2a709d5ad5b4b06fcbf340340f0cf6cac
saniazt <saniazt@gmail.com>
2022-10-17 18:03:10 +0300

    17.10.2022 18:03

--- src/main/java/com/saniazt/springdemo/MusicPlayer.java
+++ src/main/java/com/saniazt/springdemo/MusicPlayer.java
@@ -1,14 +1,49 @@
 package com.saniazt.springdemo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
+    private List<Music> musicList = new ArrayList<>();
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+
     private Music music;
+    private String name;
+    private int volume;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
 
     // IoC
     public MusicPlayer(Music music){
         this.music = music;
     }
 
+    public MusicPlayer(){}
+    public void setMusic(Music music){
+        this.music = music;
+    }
+
     public void playMusic(){
         System.out.println("Playing: "+ music.getSong());
+    }
+    public void playMusicList(){
+        musicList.forEach(a-> System.out.println("Playing: "+a.getSong()));
     }
 }
---
+++ src/main/java/com/saniazt/springdemo/RapMusic.java
@@ -0,0 +1,8 @@
+package com.saniazt.springdemo;
+
+public class RapMusic implements Music{
+    @Override
+    public String getSong() {
+        return "My name is Slim Shady";
+    }
+}
