69185d745397509aac92502ce5e054658c8eba83
saniazt <saniazt@gmail.com>
2022-10-18 16:57:35 +0300

    18.10.2022 16:57

--- src/main/java/com/saniazt/springdemo/MusicPlayer.java
+++ src/main/java/com/saniazt/springdemo/MusicPlayer.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MusicPlayer {
+    //
     private List<Music> musicList = new ArrayList<>();
 
     public void setMusicList(List<Music> musicList) {
