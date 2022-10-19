aad1e7d16ea85cddaf6bc857c63b21cd17078782
saniazt <saniazt@gmail.com>
2022-10-19 13:25:57 +0300

    19.10.2022 13:25

--- src/main/java/com/saniazt/springdemo/TestSpring.java
+++ src/main/java/com/saniazt/springdemo/TestSpring.java
@@ -10,14 +10,14 @@ public class TestSpring {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
 
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
         System.out.println(musicPlayer.getName());
         System.out.println(musicPlayer.getVolume());
 
-        ClassicalMusic classicalMusic = context.getBean("classicalMusic", ClassicalMusic.class);
         Computer computer = context.getBean("computer",Computer.class);
 
         System.out.println(musicPlayer.playMusic());
-        System.out.println(computer.toString());
+        System.out.println(computer);
 
 
 
