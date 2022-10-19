2e7a32b4db9f2631d397236e80b5452bb13f0888
saniazt <saniazt@gmail.com>
2022-10-19 11:31:59 +0300

    19.10.2022 11:31

--- src/main/java/com/saniazt/springdemo/ClassicalMusic.java
+++ src/main/java/com/saniazt/springdemo/ClassicalMusic.java
@@ -1,15 +1,31 @@
 package com.saniazt.springdemo;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.ArrayList;
 import java.util.List;
 
+
 @Component
+@Scope("singleton")
 public class ClassicalMusic implements Music{
-    List<String> listOfClassicalSongs = List.of("Classical1","Classical2","Classical3");
+
+    @PostConstruct
+    public void doMyInit(){
+        System.out.println("Doing my Initialization");
+    }
+
+    @PreDestroy
+    public void doMyDestroy(){
+        System.out.println("Doing my destruction");
+    }
+
     @Override
     public String getSong() {
-        return listOfClassicalSongs.toString();
+        return "Hungarian Rhapsody";
     }
 }
